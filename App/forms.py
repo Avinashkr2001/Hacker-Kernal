e6fa9d82882af6c3e799f1d9a233eb9ec62a325e
Avinash <2019avinash2001@gmail.com>
2024-07-20 10:17:11 +0530

    added sweetalert for better notification

--- App/forms.py
+++ App/forms.py
@@ -1,9 +1,5 @@
 from django import forms
 from .models import Author,Book
-authors=Author.objects.all()
-author_choices=[(author.id,author.name) for author in authors]
-books=Book.objects.all()
-book_choices=[(book.id,book.title) for book in books]
 class author_form(forms.Form):
     name = forms.CharField(label='Name')
     email = forms.CharField(label='Email')
@@ -12,11 +8,22 @@ class book_form(forms.Form):
     title = forms.CharField(label='Title')
     genre=forms.CharField(label='Genre')
     published_date=forms.DateField(label='Published Date',widget=forms.DateInput(attrs={'type':'date'}))
-    author=forms.ChoiceField(label='Author',choices=author_choices)
+    
+    def __init__(self, *args,**kwargs):
+        super(book_form,self).__init__(*args,**kwargs)
+        authors=Author.objects.all()
+        author_choices=[(author.id,author.name) for author in authors]
+        self.fields['author']=forms.ChoiceField(label='Author',choices=author_choices)
+    
 class record_form(forms.Form):
     user_name=forms.CharField(label='User_name')
     borrow_date=forms.CharField(label='Borrow Date',widget=forms.DateInput(attrs={'type':'date'}))
     return_date=forms.CharField(label='Return Date',widget=forms.DateInput(attrs={'type':'date'}))
-    book=forms.ChoiceField(label='Book',choices=book_choices)
+    
+    def  __init__(self, *args,**kwargs):
+        super(record_form,self).__init__(*args,**kwargs)
+        books=Book.objects.all()
+        book_choices=[(book.id,book.title) for book in books]
+        self.fields['book']=forms.ChoiceField(label='Book',choices=book_choices)
     
     
